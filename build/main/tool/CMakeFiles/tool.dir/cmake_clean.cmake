bd7fa7fb3cfca7872c76dd922733256d13f0b0a1
shuly <632725532@qq.com>
2017-07-06 13:40:16 +0800

    初步

---
+++ build/main/tool/CMakeFiles/tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tool.dir/tool.cpp.o"
+  "../../libs/libtool.pdb"
+  "../../libs/libtool.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
